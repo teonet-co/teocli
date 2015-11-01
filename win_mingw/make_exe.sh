b6b201a48e5494ca88f0c031bd7ce6ed8b7d205d
Kirill Scherba <kirill@scherba.ru>
2015-11-01 04:06:22 +0300

    Issue #117: The main_select example compiled under MinGW

--- win_mingw/make_exe.sh
+++ win_mingw/make_exe.sh
@@ -2,7 +2,9 @@ rm libteocli.dll libteocli.a teocli.exe teocli_exe.zip
 
 x86_64-w64-mingw32-gcc -o ./libteocli.dll ../libteol0/teonet_l0_client.c -DHAVE_MINGW -lws2_32 -shared -fPIC -Wl,--out-implib,libteocli.a
 #x86_64-w64-mingw32-gcc -o ./teocli.exe ../libteol0/teonet_l0_client.c ../main.c -DHAVE_MINGW -lws2_32
-x86_64-w64-mingw32-gcc -o ./teocli.exe ../main.c -DHAVE_MINGW libteocli.dll -lws2_32
 
-zip teocli_exe README teocli.exe libteocli.dll
+x86_64-w64-mingw32-gcc -o ./teocli.exe ../main.c -DHAVE_MINGW libteocli.dll -lws2_32
+x86_64-w64-mingw32-gcc -o ./teocli_s.exe ../main_select.c -DHAVE_MINGW libteocli.dll -lws2_32
+
+zip teocli_exe README teocli.exe teocli_s.exe libteocli.dll
 
