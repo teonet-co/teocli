e861717fcaa9ca529fe22d47e2a4b95c78dc174e
Kirill Scherba <kirill@scherba.ru>
2015-12-11 02:30:10 +0300

    Fixes #16: Some syntax changed in comments

--- libteol0/teonet_l0_client.h
+++ libteol0/teonet_l0_client.h
@@ -35,14 +35,14 @@
  */
 enum CMD_L {
     
-    CMD_L_ECHO = 65,         ///< Echo command
-    CMD_L_ECHO_ANSWER,       ///< Answer to echo command
-    CMD_L_PEERS = 72,        ///< Get peers command
-    CMD_L_PEERS_ANSWER,      ///< Answer to get peers command
-    CMD_L_AUTH = 77,         ///< Auth command
-    CMD_L_AUTH_ANSWER,       ///< Auth answer command
-    CMD_L_L0_CLIENTS,        ///< Get clients list command
-    CMD_L_L0_CLIENTS_ANSWER, ///< Clients list answer command
+    CMD_L_ECHO = 65,         ///< #65 Echo command
+    CMD_L_ECHO_ANSWER,       ///< #66 Answer to echo command
+    CMD_L_PEERS = 72,        ///< #72 Get peers command
+    CMD_L_PEERS_ANSWER,      ///< #73 Answer to get peers command
+    CMD_L_AUTH = 77,         ///< #77 Auth command
+    CMD_L_AUTH_ANSWER,       ///< #78 Auth answer command
+    CMD_L_L0_CLIENTS,        ///< #79 Get clients list command
+    CMD_L_L0_CLIENTS_ANSWER, ///< #80 Clients list answer command
     
     CMD_L_END = 127
 };
