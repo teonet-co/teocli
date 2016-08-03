86b94aad1686ca4a94d942e0297389b3041f95d6
Alexander Ksenofontov <aksenofo@yahoo.com>
2016-08-03 02:26:06 -0700

    initial population

---
+++ js/main.js
@@ -0,0 +1,5 @@
+console.info("node.js adapter to teo");
+
+teoclient = require("/home/aksenofo/PROJECT/TEOCLI/teocli/js/node_modules/teoclient/build/Release/teoclient.node")
+
+console.log(teoclient.version())
