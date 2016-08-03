cfe16bb9a434852416617c9bc6eb51fe0b228cbc
Alexander Ksenofontov <aksenofo@yahoo.com>
2016-08-03 02:42:46 -0700

    added init, clenup methods

--- js/main.js
+++ js/main.js
@@ -3,3 +3,5 @@ console.info("node.js adapter to teo");
 teoclient = require("/home/aksenofo/PROJECT/TEOCLI/teocli/js/node_modules/teoclient/build/Release/teoclient.node")
 
 console.log(teoclient.version())
+teoclient.init();
+teoclient.cleanup();
