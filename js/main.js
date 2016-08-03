d0fac2f0d2149e9dfd966ae903b99ba50575f6c7
Alexander Ksenofontov <aksenofo@yahoo.com>
2016-08-03 06:40:08 -0700

    added exeption class

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ teoclient = require("/home/aksenofo/PROJECT/TEOCLI/teocli/js/node_modules/teocli
 console.log(teoclient.version())
 teoclient.init();
 teoclient.cleanup();
+
+teoclient.Connector()
+
+//teoclient.connect()
+
+a = new teoclient.TeoExeption(100, "p1", "p2")
+
+a.errno = 10000
+a.call_name = "HHHHH"
+a.text = "TTTTTT"
+console.log(a.errno)
+console.log(a.call_name)
+console.log(a.text)
+console.log(a)
+
