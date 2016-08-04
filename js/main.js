824f789c238ee96ef1a0819e761a09011e996180
Alexander Ksenofontov <aksenofo@yahoo.com>
2016-08-04 02:14:03 -0700

    created native object inside other ones. Great Victory

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 console.info("node.js adapter to teo");
 
-teoclient = require("/home/aksenofo/PROJECT/TEOCLI/teocli/js/node_modules/teoclient/build/Release/teoclient.node")
-
+teoclient = require("/home/aksenofo/PROJECT/TEOCLI/teocli/js/node_modules/teoclient/build/Debug/teoclient.node")
+console.log(teoclient.version())
+try {
+    var connector = teoclient.connect("ederere", "werererer")
+//    console.log(teoclient.connect("ederere", "werererer"))
+/*
 console.log(teoclient.version())
 teoclient.init();
 teoclient.cleanup();
@@ -20,3 +24,11 @@ console.log(a.call_name)
 console.log(a.text)
 console.log(a)
 
+console.log(teoclient.connect("ederere", "werererer"))
+
+*/
+
+}
+catch (err) {
+    console.log(err)
+}
