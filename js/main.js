479d0709c047fc46cec00553e94e15e7502a27be
Alexander Ksenofontov <aksenofo@yahoo.com>
2016-08-04 06:01:41 -0700

    added init conection

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 console.info("node.js adapter to teo");
 
 teoclient = require("/home/aksenofo/PROJECT/TEOCLI/teocli/js/node_modules/teoclient/build/Debug/teoclient.node")
-console.log(teoclient.version())
+
+//m = teoclient
+
+//var p = new m.Person("Taro");
+//console.log(p.name);
+//p.name = "Jiro111";
+//console.log("dddddd", p.getName());
+
+//console.log(teoclient.version())
+
+teoclient.init();
+
 try {
-    var connector = teoclient.connect("ederere", "werererer")
+
+    var connector = teoclient.connect("127.0.0.1", 9000)
+    console.log(connector);
+    connector.login();
+
+//    var c = new teoclient.Connector("127.0.0.1", 9000);
+//    c.Login();
+
+//    console.log(JSON.stringify(connector));
+//    connector.Login();
+
 //    console.log(teoclient.connect("ederere", "werererer"))
 /*
 console.log(teoclient.version())
@@ -32,3 +53,5 @@ console.log(teoclient.connect("ederere", "werererer"))
 catch (err) {
     console.log(err)
 }
+
+teoclient.cleanup();
