e25c23e70a831b4fa51e1dfa5095095794fe0e01
kirill <kirill@scherba.ru>
2018-03-23 23:45:18 +0300

    Some minor syntax changes

--- nbind_ser/index.js
+++ nbind_ser/index.js
@@ -1,5 +1,62 @@
+'use strict';
+
+const teonet = require('./teonet');
 var nbind = require('nbind');
 var lib = nbind.init().lib;
+var Buffer = require('buffer').Buffer;
 
-lib.Teonet.hello('Hello');
+/**
+ * This application API commands
+ */
+const teoApi = {
+    /**
+     * data: 'accessToken'
+     */
+    CMD_CHECK_USER: 129
+};
 
+lib.Teonet_light.hello('Hello!');
+//lib.Teonet.start('teo-node,teo-auth', '0.0.21', 3, 5, teoEventCb);
+
+lib.Teonet_light.start(process.argv.splice(1), function(teo, event, data, data_len, user_data) {
+
+    //console.log('js event: ' + event);
+    //let rd;
+    
+    switch(event) {
+        
+        // EV_K_STARTED
+        case teonet.ev.EV_K_STARTED:
+            console.log('EV_K_STARTED');
+            break;    
+            
+        // EV_K_CONNECTED #3 New peer connected to host event
+        case teonet.ev.EV_K_CONNECTED: {                
+            let rd = new teonet.packetData(Buffer.from(data));
+            console.log('EV_K_CONNECTED, peer "' + rd.from + '" connected');
+        }   break;    
+            
+        // EV_K_IDLE
+        case teonet.ev.EV_K_IDLE:
+            console.log('EV_K_IDLE');
+            break;    
+            
+        // EV_K_RECEIVED
+        case teonet.ev.EV_K_RECEIVED: {
+            let rd = new teonet.packetData(Buffer.from(data));
+            console.log('EV_K_RECEIVED' + ', cmd: ' + rd.cmd + ', from' + rd.from, rd.data);
+            
+            switch(rd.cmd) {
+                
+                case 66: { //teoApi.CMD_CHECK_USER: {
+                    //const buf = Buffer.from('Hello\0');
+                    lib.Teonet_light.sendAnswerTo(teo);//, rd);//, rd.from);//, 'Hello\0');
+                }   break;
+                    
+                default:
+                    break
+            }
+            
+        }   break;    
+    }
+});
--- nbind_ser/libteonet-js.cpp
+++ nbind_ser/libteonet-js.cpp
@@ -4,19 +4,99 @@
  * and open the template in the editor.
  */
 
-//#include <teonet>
-
 #include <iostream>
+#include <teonet>
+
 #include "nbind/nbind.h"
 
-struct Teonet {
+class Ref {
   
-  static int hello(const char *name) {
+private:  
+  
+  void *ptr;
+  
+public:
+  
+  void *getPtr() { return ptr; }
+  void setPtr(void *p) { ptr = p; }
+};
+
+struct Teonet_light {
+
+  static void hello(const char *name) {
     // Welcome message
     std::cout << name << "\n";
   }
+
+  #define T_VERSION "0.0.1"
+
+  static int start(std::vector<std::string> arg, nbind::cbFunction &cbnbind) {
+
+    std::cout << "Teo example ver " T_VERSION ", "
+                 "based on teonet ver. " VERSION "\n";
+    
+    // Vector to argc argv
+    int i = 0, argc = arg.size();
+    char** argv = (char**)malloc(argc * sizeof(char*));
+    for(auto const& value: arg) {
+      argv[i++] = strdup(value.c_str());
+    }
+    
+    // Store callback
+    static nbind::cbFunction &cb = cbnbind;
+
+    // Application parameters
+    const char *app_argv[] = { "", "teodb_peer"};
+    teo::teoAppParam app_param = { 2, app_argv, NULL };
+
+    // Initialize teonet event manager and Read configuration
+    auto *teo = new teo::Teonet(argc, argv, [](teo::Teonet &teo, 
+      teo::teoEvents event, void *data, size_t data_len, void *user_data) {
+      
+      std::vector<unsigned char> d(static_cast<char*>(data), static_cast<char*>(data) + data_len);
+      cb.call<void>(teo, (int) event, d, data_len, 0);
+      
+    }, READ_OPTIONS|READ_CONFIGURATION|APP_PARAM, 0, &app_param);
+
+    teo->getKe()->user_data = (void*)&cbnbind; 
+    
+    // Set application type
+    teo->setAppType("teo-nbind-ex");
+    teo->setAppVersion(T_VERSION);
+
+    // Start teonet
+    teo->run();
+
+    delete teo;    
+    
+    // Free argv memory
+    for(int i = 0; i < argc; i++) {
+      free((void*)argv[i]);
+    }
+    free((void*)argv);
+
+    return (EXIT_SUCCESS);
+  }
+  
+  static int sendAnswerTo(teo::Teonet &teo) { //, teo::teoPacket &rd){//, const char *to) {//, const std::vector<unsigned char> &buf) {
+    
+    return 0;
+  }
+
 };
 
-NBIND_CLASS(Teonet) {
+//NBIND_CLASS(Ref) {
+//  construct<>();
+//  getset(getPtr, setPtr);
+//}
+
+NBIND_CLASS(Teonet_light) {
   method(hello);
+  method(start);
+  method(sendAnswerTo);
+}
+
+using namespace teo;
+NBIND_CLASS(Teonet) {
+  method(sendAnswerTo);
 }
