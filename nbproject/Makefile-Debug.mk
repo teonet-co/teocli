cf3f237ebd45e1971445f2923f54cd7a96a8f1f3
kirill <kirill@scherba.ru>
2018-03-27 01:38:37 +0300

    Netbeans project files updated

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -37,8 +37,6 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 OBJECTFILES= \
 	${OBJECTDIR}/libteol0/teonet_l0_client.o \
 	${OBJECTDIR}/main_select_cpp.o
-	#${OBJECTDIR}/main_cpp.o \
-	#${OBJECTDIR}/main_select_trudp.o
 
 
 # C Compiler Flags
@@ -70,20 +68,10 @@ ${OBJECTDIR}/libteol0/teonet_l0_client.o: libteol0/teonet_l0_client.c
 	${RM} "$@.d"
 	$(COMPILE.c) -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/libteol0/teonet_l0_client.o libteol0/teonet_l0_client.c
 
-${OBJECTDIR}/main_cpp.o: main_cpp.cpp
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -g -DHAVE_MINGW -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main_cpp.o main_cpp.cpp
-
 ${OBJECTDIR}/main_select_cpp.o: main_select_cpp.cpp
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -DHAVE_MINGW -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main_select_cpp.o main_select_cpp.cpp
-
-#${OBJECTDIR}/main_select_trudp.o: main_select_trudp.c
-#	${MKDIR} -p ${OBJECTDIR}
-#	${RM} "$@.d"
-#	$(COMPILE.c) -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main_select_trudp.o main_select_trudp.c
 
 # Subprojects
 .build-subprojects:
