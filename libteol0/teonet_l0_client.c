14f1deefa311e2552b00d5f7e0e2374b876b841c
max <mpano91@gmail.com>
2020-09-18 13:06:30 +0300

    teonet_l0_client.c: move setnonck func outside

--- libteol0/teonet_l0_client.c
+++ libteol0/teonet_l0_client.c
@@ -1462,6 +1462,7 @@ teoLNullConnectData *teoLNullConnectE(const char *server, int16_t port,
         // Connect to UDP
         int port_local = 0;
         con->fd = trudpUdpBindRaw(server, &port_local, 1);
+        trudpUdpSetNonblock(con->fd);
         if (con->fd < 0) {
             LTRACK_E("TeonetClient", "Failed to bind UDP socket.");
             con->status = CON_STATUS_SOCKET_ERROR;
